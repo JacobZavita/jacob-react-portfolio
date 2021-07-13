3bf06416f921bb2cdccf008986a37023bc114e25
Jacob Zavita <jacobzavita@gmail.com>
2021-07-13 16:20:39 -0700

    all six projects on portfolio

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -34,6 +34,7 @@ const Portfolio = () => {
       </Typography>
       <br></br>
     <Container maxWidth='lg' style={{ display: 'flex' }}>
+      {/* 1 - Currency Converter */}
       <Card className={classes.root} style={{ margin: '15px' }}>
         <CardActionArea>
           <CardMedia
@@ -57,12 +58,13 @@ const Portfolio = () => {
             </Link>
           </Button>
           <Button size="small" color="primary">
-            <Link href='https://charlottefrancis.github.io/CurrencyConverter/index.html'>
+            <Link href='https://github.com/CharlotteFrancis/CurrencyConverter'>
               github repo
             </Link>
           </Button>
         </CardActions>
       </Card>
+      {/* 2 - Password Generator */}
       <Card className={classes.root} style={{ margin: '15px' }}>
         <CardActionArea>
           <CardMedia
@@ -92,6 +94,7 @@ const Portfolio = () => {
           </Button>
         </CardActions>
       </Card>
+      {/* 3 - ComicCol */}
       <Card className={classes.root} style={{ margin: '15px' }}>
         <CardActionArea>
           <CardMedia
@@ -122,6 +125,88 @@ const Portfolio = () => {
         </CardActions>
       </Card>
     </Container>
+    <Container maxWidth='lg' style={{ display: 'flex' }}>
+      {/* 4 - eComm Backend */}
+        <Card className={classes.root} style={{ margin: '15px' }}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image="https://i.ibb.co/fdJrQy3/pexels-lukas-574071.jpg"
+              title="eCommerce Back-End"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                eCommerce Back-End
+          </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                A back end for an e-commerce website managing inventory with products, categories, and tags.
+          </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              <Link href='https://charlottefrancis.github.io/CurrencyConverter/index.html'>
+                Check it out on github
+            </Link>
+            </Button>
+          </CardActions>
+        </Card>
+      {/* 5 - Regex tutorial */}
+        <Card className={classes.root} style={{ margin: '15px' }}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image="https://i.ibb.co/1XxYtJn/pexels-luis-gomes-546819.jpg"
+              title="Regex Tutorial - Email Validation"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Regex Tutorial - Email Validation
+          </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                A GitHub gist tutorial on how to use a regex for email validation
+          </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              <Link href='https://gist.github.com/alwayssmellitfirst/d67af6b613402e3f6ae6d9726dae0d68'>
+                Check it out on github
+            </Link>
+            </Button>
+          </CardActions>
+        </Card>
+      {/* 6 - Code Quiz */}
+        <Card className={classes.root} style={{ margin: '15px' }}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image="https://i.ibb.co/h880fMk/pexels-christina-morillo-1181675.jpg"
+              title="Code Quiz"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Code Quiz - Javascript
+          </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                A quiz on how to use Javascript basics
+          </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              <Link href='https://alwayssmellitfirst.github.io/javascript_quiz/'>
+                See it live
+            </Link>
+            </Button>
+            <Button size="small" color="primary">
+              <Link href='https://github.com/alwayssmellitfirst/javascript_quiz'>
+                github repo
+            </Link>
+            </Button>
+          </CardActions>
+        </Card>
+    </Container>  
    </>
   )
 }
