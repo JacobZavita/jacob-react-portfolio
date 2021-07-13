c028a3304317708eae0d8165e4d8789d5d6d1d09
Jacob Zavita <jacobzavita@gmail.com>
2021-07-12 20:45:05 -0700

    about page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,9 @@ import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
 import Link from '@material-ui/core/Link'
+import TwitterIcon from '@material-ui/icons/Twitter'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import GitHubIcon from '@material-ui/icons/GitHub'
 
 
 const Footer = () => {
@@ -18,16 +21,22 @@ const Footer = () => {
         <Container maxWidth="lg">
           <Grid container spacing={5}>
             <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}>Github</Box>
-                <Link href='https://github.com/alwayssmellitfirst' color='#ffffff'>AlwaysSmellItFirst</Link>
+              <Box marginBottom='10px'>Check out my work</Box>
+                <Link href='https://github.com/alwayssmellitfirst' color='#ffffff'>
+                  <GitHubIcon></GitHubIcon>
+                </Link>
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}>LinkedIn</Box>
-              <Link href='https://www.linkedin.com/in/jacob-zavita-34518b13/' color='#ffffff'>Jacob Zavita</Link>
+              <Box>Take a look at my work history</Box>
+              <Link href='https://www.linkedin.com/in/jacob-zavita-34518b13/' color='#ffffff'>
+                <LinkedInIcon></LinkedInIcon>
+              </Link>
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Box borderBottom={1}>Twitter</Box>
-                <Link href='https://twitter.com/NewDevJourney' color='#ffffff'>@NewDevJourney</Link>
+              <Box>Follow me as I learn @NewDevJourney</Box>
+                <Link href='https://twitter.com/NewDevJourney' color='#ffffff'>
+                  <TwitterIcon></TwitterIcon>
+                </Link>
             </Grid>
           </Grid>
         </Container>
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,6 +1,53 @@
+import '@fontsource/roboto'
+import Container from '@material-ui/core/Container'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      // width: theme.spacing(80),
+      // height: theme.spacing(50),
+    },
+  },
+}));
+
 const About = () => {
+  const classes = useStyles()
+
   return (
-    <h1>This is the About page.</h1>
+    <>
+      <Container
+        maxWidth='lg'
+        className={classes.root}
+        style={{ display: 'flex', justifyContent: 'center' }}
+      >
+        <Typography 
+          variant='h2'
+          marginLeft='15px'
+          marginBottom='20px'
+        >
+          Hi, I'm Jacob. I'm a...
+          </Typography>
+        <Paper elevation={3} style={{ padding: '15px', display: 'flex' }}>
+          <Typography variant='h4'>
+            Fullstack developer in training with a background in SaaS sales. I'm building out my portfolio as I finish up UCI's coding bootcamp so hang tight for more updates.
+            <br/>
+            <br/>
+            Also an avid runner and mountain biker who knows way too much about Star Wars, Marvel, and the Cosmere so feel free to geek out.
+            <br/>
+            <br/>
+            Interested in all things fin-tech, B2B Marketing and RevOps, blockchain, smart contracts, Ethereum and DeFi.
+          </Typography>
+          <img src='https://i.postimg.cc/Njfzsfyv/00100lr-PORTRAIT-00100-BURST20200828161547179-COVER.jpg' alt='Jacob Zavita' />
+        </Paper>
+
+      </Container>
+    </>
   )
 }
 
