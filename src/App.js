bf420abfc9904365262560ee0013f24cc5c50fae
Jacob Zavita <jacobzavita@gmail.com>
2021-07-14 09:41:49 -0700

    deploy fix

--- src/App.js
+++ src/App.js
@@ -20,16 +20,16 @@ const App = () => {
       <div>
         <Appbar />
         <Switch>
-          <Route exact path='/'>
+          <Route exact path='/jacob-react-portfolio/'>
             <About />
           </Route>
-          <Route path='/portfolio'>
+          <Route path='/jacob-react-portfolio/portfolio'>
             <Portfolio />
           </Route>
-          <Route path='/contact'>
+          <Route path='/jacob-react-portfolio/contact'>
             <Contact />
           </Route>
-          <Route path='/resume'>
+          <Route path='/jacob-react-portfolio/resume'>
             <Resume />
           </Route>
         </Switch>
